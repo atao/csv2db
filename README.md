e5e4f84d2ef43604c0cd9543ea9396455d87ce7b
ATAO <atao@users.noreply.github.com>
2024-01-16 11:56:49 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# csv2db
+📋 Convert CSV to SQLite Database.
+
+## Purpose
+
+Easily transform a csv file into a SQLite database.
+
+## Requirements
+```
+pip install -r requirements.txt
+```
+
+## Usage and options
+
+```
+Usage: csv2db.py [OPTIONS]
+
+Options:
+  --version                       Show the version and exit.
+  -i, --csvfile TEXT              CSV file  [required]
+  -d, --database TEXT             Database name.  [default: database.db]
+  -t, --table TEXT                Table name.  [default: data]
+  -m, --mode [fail|replace|append]
+                                  if data already exist do  [default: fail]
+  -v, --verbose                   Verbose mode.
+  -h, --help                      Show this message and exit.
+```
+
+## Quickstart
+Just run for example :
+```python
+csv2db.py --csvfile mycsv.csv --database mycsv.db --table mydata --mode replace --verbose
+```
---
+++ csv2db.py
@@ -0,0 +1,47 @@
+import sqlite3
+import sys
+import click
+import pandas as pd
+
+
+@click.command(context_settings=dict(help_option_names=['-h', '--help']))
+@click.version_option(version='1', prog_name="Convert CSV to SQLite Database.")
+@click.option('--csvfile', '-i', help='CSV file', required=True, type=str)
+@click.option('--database', '-d', default='database.db', help='Database name.', show_default=True, type=str)
+@click.option('--table', '-t', default='data', help='Table name.', show_default=True, type=str)
+@click.option('--mode', '-m', type=click.Choice(['fail', 'replace', 'append'], case_sensitive=False),
+              help='if data already exist do...', default='fail', show_default=True)
+@click.option('--verbose', '-v', is_flag=True, help="Verbose mode.")
+def cli(verbose, csvfile, database, table, mode):
+    # Check args
+    if not database.endswith(".db"):
+        database = '{}.db'.format(database)
+    if not csvfile.endswith(".csv"):
+        csvfile = '{}.csv'.format(csvfile)
+    if mode:
+        mode = mode.lower()
+
+    # Import data in DataFrame
+    if verbose:
+        print("[+] Reading CSV file...")
+    df = pd.read_csv(csvfile)
+
+    # Insert to database
+    if verbose:
+        print("[+] Insert into {}...".format(database))
+    conn = sqlite3.connect(database)
+    try:
+        df.to_sql(con=conn, name=table, if_exists=mode)
+    except ValueError as e:
+        print("[!] Error : {}".format(e))
+        exit(1)
+    except:
+        print("Error")
+        exit(2)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        cli.main(['--help'])
+    else:
+        cli()
